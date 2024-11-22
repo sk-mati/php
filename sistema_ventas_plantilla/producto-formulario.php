a0a9585cb3963c327579fcab337d7e0ef9339be9
sk-mati <mati.skmj@gmail.com>
2024-11-21 22:11:30 -0300

    changes_21_11

--- sistema_ventas_plantilla/producto-formulario.php
+++ sistema_ventas_plantilla/producto-formulario.php
@@ -2,54 +2,39 @@
 <?php
 
 include_once "config.php";
-include_once "entidades/cliente.php";
-include_once "entidades/venta.php";
-include_once "entidades/provincia.php";
-include_once "entidades/localidad.php";
+include_once "entidades/producto.php";
+include_once "entidades/tipoproducto.php";
 
-$cliente = new Cliente();
-$cliente->cargarFormulario($_REQUEST);
+$producto = new Producto();
 
-$pg = "Listado de clientes";
+if($_POST){
 
-if ($_POST) {
-    if (isset($_POST["btnGuardar"])) {
-        if (isset($_GET["id"]) && $_GET["id"] > 0) {
-            //Actualizo un cliente existente
-            $cliente->actualizar();
+    if(isset($_POST["btnGuardar"])){
+        $producto->cargarFormulario($_REQUEST);
+
+        if(isset($_GET["id"]) && $_GET["id"] > 0){
+            $producto->actualizar();
+            $msg["texto"] = "Actualizado correctamente";
+            $msg["codigo"] = "alert-success";
         } else {
-            //Es nuevo
-            $cliente->insertar();
+            $producto->insertar();
+            $msg["texto"] = "Insertado correctamente";
+            $msg["codigo"] = "alert-success";
         }
-        $msg["texto"] = "Guardado correctamente";
-        $msg["codigo"] = "alert-success";
-
-    } else if (isset($_POST["btnBorrar"])) {
-        //Si existen ventas asociadas al cliente que se intenta eliminar, muestra mensaje de alerta
-        $venta = new Venta();
-        if ($venta->obtenerVentasPorCliente($cliente->idcliente)) {
-            $msg["texto"] = "No se puede eliminar un cliente con ventas asociadas.";
-            $msg["codigo"] = "alert-danger";
-        } else {
-            $cliente->eliminar();
-            header("Location: cliente-listado.php");
-        }
+    } else if(isset($_POST["btnBorrar"])) {
+        $producto->cargarFormulario($_REQUEST);
+        $producto->eliminar();
+        header("Location: producto-listado.php");
     }
 }
 
-if (isset($_GET["do"]) && $_GET["do"] == "buscarLocalidad" && $_GET["id"] && $_GET["id"] > 0) {
-    $idProvincia = $_GET["id"];
-    $localidad = new Localidad();
-    $aLocalidad = $localidad->obtenerPorProvincia($idProvincia);
-    echo json_encode($aLocalidad);
-    exit;
-}
-if (isset($_GET["id"]) && $_GET["id"] > 0) {
-    $cliente->obtenerPorId();
+if(isset($_GET["id"]) && $_GET["id"] > 0){
+    $producto->cargarFormulario($_REQUEST);
+    $producto->obtenerPorId();
 }
 
-$provincia = new Provincia();
-$aProvincias = $provincia->obtenerTodos();
+$tipoProducto = new TipoProducto ();
+$aTipoProductos = $tipoProducto -> obtenerTodos();
 
 include_once "header.php";
 ?>
@@ -71,7 +56,7 @@ include_once "header.php";
 	<body>
 		<div class="main-container">
 			<div id="editor">
-				<p>Hello from CKEditor 5!</p>
+				<p></p>
 			</div>
 		</div>
 		<script type="importmap">
@@ -121,9 +106,18 @@ include_once "header.php";
 
           <!-- Page Heading -->
           <h1 class="h3 mb-4 text-gray-800">Productos</h1>
+          <?php if (isset($msg)): ?>
+            <div class="row">
+                <div class="col-12">
+                    <div class="alert <?php echo $msg["codigo"]; ?>" role="alert">
+                        <?php echo $msg["texto"]; ?>
+                    </div>
+                </div>
+            </div>
+            <?php endif;?>
             <div class="row">
                 <div class="col-12 mb-3">
-                    <a href="productos.php" class="btn btn-primary mr-2">Listado</a>
+                    <a href="producto-listado.php" class="btn btn-primary mr-2">Listado</a>
                     <a href="producto-formulario.php" class="btn btn-primary mr-2">Nuevo</a>
                     <button type="submit" class="btn btn-success mr-2" id="btnGuardar" name="btnGuardar">Guardar</button>
                     <button type="submit" class="btn btn-danger" id="btnBorrar" name="btnBorrar">Borrar</button>
@@ -132,25 +126,28 @@ include_once "header.php";
             <div class="row">
                 <div class="col-6 form-group">
                     <label for="txtNombre">Nombre:</label>
-                    <input type="text" required class="form-control" name="txtNombre" id="txtNombre">
+                    <input type="text" required class="form-control" name="txtNombre" id="txtNombre" value="<?php echo $producto->nombre; ?>">
                 </div>
                 <div class="col-6 form-group">
                     <label for="txtNombre">Tipo de producto:</label>
                     <select name="lstTipoProducto" id="lstTipoProducto" class="form-control" >
                         <option value="" disabled selected>Seleccionar</option>
+                        <?php foreach($aTipoProductos as $tipoProducto): ?>
+                            <option value="<?php echo $tipoProducto->idtipoproducto; ?>"><?php echo $tipoProducto->nombre; ?></option>
+                        <?php endforeach; ?>
                     </select>
                 </div>
                 <div class="col-6 form-group">
                     <label for="txtCantidad">Cantidad:</label>
-                    <input type="number" class="form-control" name="txtCantidad" id="txtCantidad" required>
+                    <input type="number" class="form-control" name="txtCantidad" id="txtCantidad" required value="<?php echo $producto->cantidad; ?>">
                 </div>
                 <div class="col-6 form-group">
                     <label for="txtPrecio">Precio:</label>
-                    <input type="text" class="form-control" name="txtPrecio" id="txtPrecio">
+                    <input type="text" class="form-control" name="txtPrecio" id="txtPrecio" value="<?php echo $producto->precio; ?>">
                 </div>
                 <div class="col-12 form-group">
                     <label for="txtCorreo">Descripción:</label>
-                    <textarea type="text" name="txtDescripcion" id="txtDescripcion"></textarea>
+                    <textarea type="text" name="txtDescripcion" id="txtDescripcion"><?php echo $producto->descripcion; ?></textarea>
                 </div>
                 <div class="col-6 form-group">
                     <label for="fileImagen">Imagen:</label>
--- sistema_ventas_plantilla/producto-listado.php
+++ sistema_ventas_plantilla/producto-listado.php
@@ -1,11 +1,12 @@
 <?php
 
 include_once "config.php";
-include_once "entidades/cliente.php";
+include_once "entidades/producto.php";
+
 $pg = "Listado de productos";
 
-$cliente = new Cliente();
-$aClientes = $cliente->obtenerTodos();
+$producto = new Producto ();
+$aProductos = $producto -> obtenerTodos();
 
 include_once("header.php"); 
 ?>
@@ -16,7 +17,7 @@ include_once("header.php");
           <h1 class="h3 mb-4 text-gray-800">Listado de productos</h1>
           <div class="row">
                 <div class="col-12 mb-3">
-                    <a href="cliente-formulario.php" class="btn btn-primary mr-2">Nuevo</a>
+                    <a href="producto-formulario.php" class="btn btn-primary mr-2">Nuevo</a>
                 </div>
             </div>
           <table class="table table-hover border">
@@ -27,15 +28,14 @@ include_once("header.php");
                 <th>Precio</th>
                 <th>Acciones</th>
             </tr>
-            <?php foreach ($aClientes as $cliente): ?>
+            <?php foreach ($aProductos as $producto): ?>
               <tr>
-                  <td><?php echo $cliente->cuit; ?></td>
-                  <td><?php echo $cliente->nombre; ?></td>
-                  <td><?php echo date_format(date_create($cliente->fecha_nac), "d/m/Y"); ?></td>
-                  <td><?php echo $cliente->telefono; ?></td>
-                  <td><?php echo $cliente->correo; ?></td>
+                  <td><img src="files/<?php echo $producto->imagen; ?>" class="img_thumbnail"></td>
+                  <td><?php echo $producto->nombre; ?></td>
+                  <td><?php echo $producto->cantidad; ?></td>
+                  <td><?php echo number_format($producto->precio, 2, ",", "."); ?></td>
                   <td style="width: 110px;">
-                      <a href="cliente-formulario.php?id=<?php echo $cliente->idcliente; ?>"><i class="fas fa-search"></i></a>   
+                      <a href="producto-formulario.php?id=<?php echo $producto->idproducto; ?>"><i class="fas fa-search"></i></a>   
                   </td>
               </tr>
             <?php endforeach; ?>
